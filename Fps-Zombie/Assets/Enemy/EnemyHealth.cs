faa8dada067a9e6a920dea2c7e513e10a05fe470
k6ss21 <61103379+k6ss21@users.noreply.github.com>
2021-11-28 18:56:13 +0530

    Shooting

---
+++ Fps-Zombie/Assets/Enemy/EnemyHealth.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+    [SerializeField] float currentHealth;
+    private void Awake()
+    {
+        currentHealth = health;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        Debug.Log("health" + currentHealth);
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
